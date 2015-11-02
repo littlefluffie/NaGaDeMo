e7b9d7cf99784ee0e2c6aa714f7164f96777d703
Donovan Jordaan <littlefluffie@live.co.za>
2015-11-02 08:47:47 +0200

    Initial commit

---
+++ NaGaDeMo/Map.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace NaGaDeMo
+{
+    public class Tile
+    {
+        public Rectangle bounds = new Rectangle(0, 0, 64, 64);
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(background, this.bounds, Color.White);
+        }
+
+    }
+
+    public class Map
+    {
+        public Tile[] tiles;
+
+        public Texture2D mapImage;
+
+        public void renderMap()
+        {
+            foreach (Tile tile in this.tiles)
+            {
+
+            }
+        }
+    }
+}
+
---
+++ NaGaDeMo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NaGaDeMo")]
+[assembly: AssemblyProduct("NaGaDeMo")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b972e0fc-4a0c-496f-b8eb-cb6bcea6a386")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.4.0.456")]
+[assembly: AssemblyFileVersion("3.4.0.456")]
